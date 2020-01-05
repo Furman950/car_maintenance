57a0182d4cd6f3c11ca9ef3cb5977e3bc1665255
Fermin Sandoval <ferminsandoval950@hotmail.com>
2020-01-04 18:14:36 -0700

    Implemented creating oil change for car

--- lib/core/viewmodels/views/oil_change_form_view_model.dart
+++ lib/core/viewmodels/views/oil_change_form_view_model.dart
@@ -1,11 +1,25 @@
+import 'package:car_maintenance/core/models/oil_change.dart';
 import 'package:car_maintenance/core/services/car_api.dart';
 import 'package:car_maintenance/core/viewmodels/base_model.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
 
 class OilChangeFormViewModel extends BaseModel {
   final CarApi _carApi;
+  int _mileage = 0;
+  String vin;
+  OilChange oilChange;
 
   OilChangeFormViewModel({@required CarApi carApi}) : _carApi = carApi;
 
-  saveOilChange() async {}
+  Future<Response> saveOilChange(DateTime dateTime) async {
+    oilChange = OilChange(vin: vin, mileage: _mileage, dateTime: dateTime);
+    return _carApi.saveOilChange(oilChange);
+  }
+
+  onChange(String value) {
+    _mileage = int.tryParse(value);
+  }
+
+  setVin(String vin) => this.vin = vin;
 }
--- lib/ui/views/OilChangeForm/oil_change_form.dart
+++ lib/ui/views/OilChangeForm/oil_change_form.dart
@@ -3,9 +3,7 @@ import 'package:car_maintenance/core/viewmodels/views/oil_change_form_view_model
 import 'package:car_maintenance/ui/views/base_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as prefix0;
 import 'package:flutter/services.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class OilChangeForm extends StatefulWidget {
@@ -37,8 +35,7 @@ class _OilChangeFormState extends State<OilChangeForm> {
 
   _selectTime(BuildContext context) async {
     final TimeOfDay time = await showTimePicker(
-        context: context,
-        initialTime: prefix0.TimeOfDay.fromDateTime(_dateTime));
+        context: context, initialTime: TimeOfDay.fromDateTime(_dateTime));
     print(time);
 
     if (time != null) {
@@ -54,41 +51,69 @@ class _OilChangeFormState extends State<OilChangeForm> {
   Widget build(BuildContext context) {
     return BaseWidget<OilChangeFormViewModel>(
       model: OilChangeFormViewModel(carApi: Provider.of(context)),
+      onModelReady: (model) => model.setVin(widget.vin),
       builder: (context, model, child) => Scaffold(
         appBar: AppBar(
           title: const Text('Add oil change to Car'),
         ),
-        body: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Enter Mileage',
-                    hintText: 'Enter current vehicle mileage'),
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  WhitelistingTextInputFormatter.digitsOnly
-                ],
-              ),
-              Text('Date ${formatDate(_dateTime)}'),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RaisedButton(
-                    child: const Text('Change date'),
-                    onPressed: () => _selectDate(context),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  RaisedButton(
-                    child: const Text('Change time'),
-                    onPressed: () => _selectTime(context),
-                  )
-                ],
-              )
-            ],
+        body: Container(
+          margin: EdgeInsets.only(left: 10, right: 10),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: 'Enter Mileage',
+                      hintText: 'Enter current vehicle mileage'),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    WhitelistingTextInputFormatter.digitsOnly
+                  ],
+                  onChanged: model.onChange,
+                  validator: (value) =>
+                      value.isEmpty ? 'This value is required!' : null,
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  'Date ${formatDate(_dateTime)}',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RaisedButton(
+                      child: const Text('Change date'),
+                      onPressed: () => _selectDate(context),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    RaisedButton(
+                      child: const Text('Change time'),
+                      onPressed: () => _selectTime(context),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                RaisedButton(
+                  child: const Text('Save Oil Change'),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      var res = await model.saveOilChange(_dateTime);
+                      if (res.statusCode == 201){
+                        Navigator.pop(context, model.oilChange);
+                      }
+
+                    }
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
